274294f447ac20e771886dc11e8e118b36fc2fad
Nicolas Lurkin <nicolas.lurkin@cern.ch>
2023-04-12 20:19:31 +0200

    Set tree name as unique index

--- BonsaiBuddy_web/TreeInfo/models.py
+++ BonsaiBuddy_web/TreeInfo/models.py
@@ -9,9 +9,12 @@ class MyModel(models.Model):
         )
 
 class TreeInfo(mongoengine.Document):
-    name = mongoengine.StringField(max_length=200, required=True, index=True)
+    name = mongoengine.StringField(max_length=200, required=True, index=True, unique=True)
     latin_name = mongoengine.StringField(max_length=200)
     description = mongoengine.StringField()
     published = mongoengine.BooleanField(default=False)
 
     meta = {'db_alias': 'mongo', "indexes": ["$name"]}
+
+    def __str__(self):
+        return self.name
