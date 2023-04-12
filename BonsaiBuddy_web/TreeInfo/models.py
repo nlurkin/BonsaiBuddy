af7bd35758ec056a12196d14a03a62274864855d
Nicolas Lurkin <nicolas.lurkin@cern.ch>
2023-04-12 20:19:11 +0200

    Remove modification functions from TreeInfo (display only)

--- BonsaiBuddy_web/TreeInfo/models.py
+++ BonsaiBuddy_web/TreeInfo/models.py
@@ -1,38 +1,6 @@
 import mongoengine
-import datetime
-from django.utils import timezone
-from django.urls import reverse
 from django.db import models
 
-class Question(mongoengine.Document):
-    question_text = mongoengine.StringField(max_length=200)
-    pub_date = mongoengine.DateTimeField("date published")
-    meta = {'db_alias': 'mongo'}
-
-    def __str__(self):
-        return self.question_text
-
-    def was_published_recently(self):
-        now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now
-
-    @property
-    def choices(self):
-        return Choice.objects(question=self).all()
-
-    def get_absolute_url(self):
-        return reverse('TreeInfo:detail', kwargs={'pk': self.pk})
-
-
-
-class Choice(mongoengine.Document):
-    question = mongoengine.ReferenceField(Question, reverse_delete_rule=mongoengine.CASCADE)
-    choice_text = mongoengine.StringField(max_length=200)
-    votes = mongoengine.IntField(default=0)
-    meta = {'db_alias': 'mongo'}
-
-    def __str__(self):
-        return self.choice_text
 
 class MyModel(models.Model):
     class Meta:
--- BonsaiBuddy_web/TreeInfo/urls.py
+++ BonsaiBuddy_web/TreeInfo/urls.py
@@ -5,8 +5,5 @@ from . import views
 app_name = "TreeInfo"
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
-    path("create/", views.CreateFormView.as_view(), name="create"),
     path("<pk>/", views.DetailView.as_view(), name="detail"),
-    path("<pk>/results/", views.ResultsView.as_view(), name="results"),
-    path("<pk>/vote/", views.VoteView.as_view(), name="vote"),
 ]
