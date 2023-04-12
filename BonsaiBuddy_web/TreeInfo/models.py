bb2427f2ea8b268ebf92605a9556b0d8f99bb2dd
Nicolas Lurkin <nicolas.lurkin@cern.ch>
2023-04-12 17:00:01 +0200

    Update models to add actual first model

--- BonsaiBuddy_web/TreeInfo/models.py
+++ BonsaiBuddy_web/TreeInfo/models.py
@@ -40,3 +40,10 @@ class MyModel(models.Model):
             ('change_content', 'Content administrators'),
         )
 
+class TreeInfo(mongoengine.Document):
+    name = mongoengine.StringField(max_length=200, required=True, index=True)
+    latin_name = mongoengine.StringField(max_length=200)
+    description = mongoengine.StringField()
+    published = mongoengine.BooleanField(default=False)
+
+    meta = {'db_alias': 'mongo', "indexes": ["$name"]}
