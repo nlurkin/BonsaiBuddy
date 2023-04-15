89b5141a18e22b0d2e66432219fe10477e27948f
Nicolas Lurkin <nicolas.lurkin@cern.ch>
2023-04-15 17:08:07 +0200

    Use permissions in menu

--- BonsaiBuddy_web/BonsaiBuddy/menus.py
+++ BonsaiBuddy_web/BonsaiBuddy/menus.py
@@ -1,12 +1,13 @@
 class MenuItem(object):
-    def __init__(self, display, urlref=None, submenu=None):
+    def __init__(self, display, urlref=None, submenu=None, permission=None):
         if submenu:
             self.itype = "submenu"
         else:
-            self. itype = "menuitem"
-        self. urlref = urlref
-        self. display = display
-        self. submenu = submenu
+            self.itype = "menuitem"
+        self.urlref = urlref
+        self.display = display
+        self.submenu = submenu
+        self.permission = permission
 
 class MenuMixin(object):
     menu_context = []
--- BonsaiBuddy_web/TreeInfo/menu.py
+++ BonsaiBuddy_web/TreeInfo/menu.py
@@ -3,4 +3,5 @@ from BonsaiBuddy.menus import MenuItem, MenuMixin
 class TreeInfoMenuMixin(MenuMixin):
     menu_context = {"menu_items": [
         MenuItem("TreeInfo", "TreeInfo:index"),
+        MenuItem("Admin", "BonsaiAdmin:index", permission="TreeInfo.change_content"),
     ]}
