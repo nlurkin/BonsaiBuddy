f001b9419b070fce3208a845c4f90e27fe254910
Nicolas Lurkin <nicolas.lurkin@cern.ch>
2023-04-15 16:00:53 +0200

    Add admin index link

--- BonsaiBuddy_web/BonsaiAdmin/menu.py
+++ BonsaiBuddy_web/BonsaiAdmin/menu.py
@@ -1,6 +1,7 @@
 from BonsaiBuddy.menus import MenuItem
 
 menu_context = {"menu_items": [
+    MenuItem("Admin", "BonsaiAdmin:index"),
     MenuItem("TreeInfo", submenu=[
         MenuItem("Create", "BonsaiAdmin:treeinfo_create")
     ])
