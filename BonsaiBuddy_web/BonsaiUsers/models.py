e92d82855bd547d351119587b6c91e00996643cb
Nicolas Lurkin <nicolas.lurkin@cern.ch>
2023-04-23 16:05:46 +0200

    Add user profile class

--- BonsaiBuddy_web/BonsaiUsers/models.py
+++ BonsaiBuddy_web/BonsaiUsers/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+import mongoengine
 
 # Create your models here.
 from django.contrib.auth.models import AbstractUser
@@ -6,3 +6,15 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+COUNTRY_ENUM = ["Undefined", "Belgium"]
+
+class UserProfile(mongoengine.Document):
+    username = mongoengine.StringField()
+    password = mongoengine.StringField(max_length=128)
+    salt = mongoengine.StringField(max_length=16)
+    last_pwd_update = mongoengine.DateTimeField(null=True)
+    last_login = mongoengine.DateTimeField(null=True)
+    country = mongoengine.EnumField(enum=COUNTRY_ENUM, default="Undefined")
+
+    meta = {'db_alias': 'mongo', "indexes": ["$username"]}
