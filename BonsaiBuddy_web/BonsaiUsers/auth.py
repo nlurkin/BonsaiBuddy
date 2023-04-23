a84c11ee97f747b0afba8cc019cb677447f8cf5b
Nicolas Lurkin <nicolas.lurkin@cern.ch>
2023-04-23 17:01:21 +0200

    Implement backend for authenticationo against mongodb profile

---
+++ BonsaiBuddy_web/BonsaiUsers/auth.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.backends import BaseBackend
+from BonsaiUsers.models import User, UserProfile
+import bcrypt
+
+
+class DjangoBackend(BaseBackend):
+    def authenticate(self, request, username=None, password=None):
+        # Check the user profile exists
+        try:
+            up = UserProfile.objects.get(username=username)
+            # Check the password
+            if not bcrypt.checkpw(password.encode("utf-8"), up.password):
+                return None
+        except UserProfile.DoesNotExist:
+            return None
+
+        # If yes, extract the django user, or create a new one
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            # Create a new user. There's no need to set a password
+            # because only the password from settings.py is checked.
+            user = User(username=username)
+            user.save()
+        return user
+
+    def get_user(self, user_id):
+        try:
+            user = User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
+        return user
