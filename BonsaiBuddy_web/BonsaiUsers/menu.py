c6581449ad2ec7bbf14a8a5dcc97527fa23d8ce5
Nicolas Lurkin <nicolas.lurkin@cern.ch>
2023-04-30 19:08:47 +0200

    Add a profile details view with the menu

---
+++ BonsaiBuddy_web/BonsaiUsers/menu.py
@@ -0,0 +1,4 @@
+from BonsaiBuddy.menus import MenuItem, MenuMixin
+
+class BonsaiUsersMenuMixin(MenuMixin):
+    menu_context = MenuMixin.get_init_menu_context()
--- BonsaiBuddy_web/BonsaiUsers/views.py
+++ BonsaiBuddy_web/BonsaiUsers/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .menu import BonsaiUsersMenuMixin
+from django.views import View, generic
+from .models import UserProfile
+from utils import get_object_or_404
+from django.http import Http404
+class DetailView(BonsaiUsersMenuMixin, View):
+    model = UserProfile
+    template_name = "BonsaiUsers/profile.html"
+    context_object_name = "profile"
+
+    def get(self, request):
+        try:
+            profile = get_object_or_404(self.model, username=request.user.username)
+        except Http404:
+            return render(request, "BonsaiUsers/not_found.html", {**self.build_menu_context(request), self.context_object_name: request.user})
+        return render(request, self.template_name, {**self.build_menu_context(request), self.context_object_name: profile})
 
 # Create your views here.
