857ce7dad3e57ebbba83f21dbf0cea5395357060
Nicolas Lurkin <nicolas.lurkin@cern.ch>
2023-04-30 19:11:36 +0200

    Add a form for signup

---
+++ BonsaiBuddy_web/BonsaiUsers/forms.py
@@ -0,0 +1,63 @@
+from django import forms
+from django.contrib.auth import password_validation
+from django.core.exceptions import ValidationError
+from .models import UserProfile
+
+class CustomUserCreationForm(forms.Form):
+    error_messages = {
+        "password_mismatch": "The two password fields didn't match.",
+        "username_exists": "The selected username already exists.",
+    }
+    username = forms.CharField()
+    password1 = forms.CharField(
+        label="Password",
+        strip=False,
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+    password2 = forms.CharField(
+        label="Password confirmation",
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
+        strip=False,
+        help_text="Enter the same password as before, for verification.",
+    )
+
+    def __init__(self, *args, **kwargs):
+        self.instance = UserProfile()
+        super().__init__(*args, **kwargs)
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError(
+                self.error_messages["password_mismatch"],
+                code="password_mismatch",
+            )
+        return password2
+
+    def clean_username(self):
+        """Reject usernames that differ only in case."""
+        username = self.cleaned_data.get("username")
+        if (username and len(UserProfile.objects(username__iexact=username))>0):
+            raise ValidationError(self.error_messages["username_exists"], code="username_exists")
+        else:
+            return username
+
+    def _post_clean(self):
+        super()._post_clean()
+        # Validate the password after self.instance is updated with form data
+        # by super().
+        password = self.cleaned_data.get("password2")
+        if password:
+            try:
+                password_validation.validate_password(password, self.instance)
+            except ValidationError as error:
+                self.add_error("password2", error)
+
+    def save(self):
+        username = self.cleaned_data.get("username")
+        password = self.cleaned_data.get("password2")
+        user = UserProfile(username=username)
+        user.create_user(password)
+        return username, password
--- BonsaiBuddy_web/BonsaiUsers/views.py
+++ BonsaiBuddy_web/BonsaiUsers/views.py
@@ -4,6 +4,10 @@ from django.views import View, generic
 from .models import UserProfile
 from utils import get_object_or_404
 from django.http import Http404
+from .forms import CustomUserCreationForm
+from django.urls import reverse_lazy
+from django.contrib.auth import authenticate, login
+
 class DetailView(BonsaiUsersMenuMixin, View):
     model = UserProfile
     template_name = "BonsaiUsers/profile.html"
@@ -16,4 +20,13 @@ class DetailView(BonsaiUsersMenuMixin, View):
             return render(request, "BonsaiUsers/not_found.html", {**self.build_menu_context(request), self.context_object_name: request.user})
         return render(request, self.template_name, {**self.build_menu_context(request), self.context_object_name: profile})
 
-# Create your views here.
+class SignupView(BonsaiUsersMenuMixin, generic.FormView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy("Profile:detail")
+    template_name = "registration/signup.html"
+
+    def form_valid(self, form):
+        username, password = form.save()
+        user = authenticate(self.request, username=username, password=password)
+        login(self.request, user)
+        return super().form_valid(form)
