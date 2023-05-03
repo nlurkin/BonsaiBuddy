bd615647ddf2602d0cb9bc2146c9c0af4f08ee99
Nicolas Lurkin <nicolas.lurkin@cern.ch>
2023-05-03 10:00:26 +0200

    Custom login class just requires the menu mixin

--- BonsaiBuddy_web/BonsaiUsers/views.py
+++ BonsaiBuddy_web/BonsaiUsers/views.py
@@ -33,10 +33,7 @@ class SignupView(BonsaiUsersMenuMixin, generic.FormView):
         return super().form_valid(form)
 
 class MyLoginView(BonsaiUsersMenuMixin, views.LoginView):
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context.update(self.build_menu_context(self.request))
-        return context
+    pass
 
 class ProfileUpdateView(BonsaiUsersMenuMixin, generic.FormView):
     success_url = reverse_lazy("Profile:detail")
