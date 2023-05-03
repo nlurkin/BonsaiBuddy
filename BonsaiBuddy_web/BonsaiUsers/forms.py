9c9fa2237efa82e854e42d583f337dddadfcd8da
Nicolas Lurkin <nicolas.lurkin@cern.ch>
2023-05-03 10:42:12 +0200

    Forms requiring new passord have common PasswordValidationClass parent
    Validation methods are also cleaned up
    Do not use _post_clean

--- BonsaiBuddy_web/BonsaiUsers/forms.py
+++ BonsaiBuddy_web/BonsaiUsers/forms.py
@@ -5,12 +5,11 @@ from .models import UserProfile
 import bcrypt
 import pycountry
 
-class CustomUserCreationForm(forms.Form):
+
+class PasswordValidationClass():
     error_messages = {
         "password_mismatch": "The two password fields didn't match.",
-        "username_exists": "The selected username already exists.",
     }
-    username = forms.CharField()
     password1 = forms.CharField(
         label="Password",
         strip=False,
@@ -23,20 +22,27 @@ class CustomUserCreationForm(forms.Form):
         strip=False,
         help_text="Enter the same password as before, for verification.",
     )
-
-    def __init__(self, *args, **kwargs):
-        self.instance = UserProfile()
-        super().__init__(*args, **kwargs)
+    def clean_password(self):
+        password = self.cleaned_data.get("password")
+        if password:
+            try:
+                password_validation.validate_password(password, None)
+            except ValidationError as error:
+                self.add_error("password2", error)
+        return password
 
     def clean_password2(self):
         password1 = self.cleaned_data.get("password1")
         password2 = self.cleaned_data.get("password2")
         if password1 and password2 and password1 != password2:
-            raise ValidationError(
-                self.error_messages["password_mismatch"],
-                code="password_mismatch",
-            )
-        return password2
+            raise ValidationError(self.error_messages["password_mismatch"], code="password_mismatch")
+
+
+class CustomUserCreationForm(PasswordValidationClass, forms.Form):
+    error_messages = {**PasswordValidationClass.error_messages,
+        "username_exists": "The selected username already exists.",
+    }
+    username = forms.CharField()
 
     def clean_username(self):
         """Reject usernames that differ only in case."""
@@ -45,17 +51,6 @@ class CustomUserCreationForm(forms.Form):
             raise ValidationError(self.error_messages["username_exists"], code="username_exists")
         else:
             return username
-
-    def _post_clean(self):
-        super()._post_clean()
-        # Validate the password after self.instance is updated with form data
-        # by super().
-        password = self.cleaned_data.get("password2")
-        if password:
-            try:
-                password_validation.validate_password(password, self.instance)
-            except ValidationError as error:
-                self.add_error("password2", error)
 
     def save(self):
         username = self.cleaned_data.get("username")
@@ -69,19 +64,9 @@ def build_country_list():
     countries.insert(0, ("unknown", "Unknown"))
     return countries
 
-class UpdateUserProfileForm(forms.Form):
-
-    country = forms.CharField(widget=forms.Select(choices=build_country_list()))
-
-    def save(self, username):
-        user = UserProfile.get_user(username)
-        user.country = self.cleaned_data.get("country")
-        user.save()
-
-class ModifyPasswordForm(forms.Form):
-    error_messages = {
-        "password_mismatch": "The two password fields didn't match.",
-        "username_exists": "The selected username already exists.",
+class ModifyPasswordForm(PasswordValidationClass, forms.Form):
+    error_messages = {**PasswordValidationClass.error_messages,
+        "incorrect_old": "The old password is incorrect.",
     }
     old_password = forms.CharField(
         label="Old password",
@@ -89,56 +74,24 @@ class ModifyPasswordForm(forms.Form):
         widget=forms.PasswordInput(attrs={"autocomplete": "old-password"}),
         help_text="Old password"
     )
-    password1 = forms.CharField(
-        label="Password",
-        strip=False,
-        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
-        help_text=password_validation.password_validators_help_text_html(),
-    )
-    password2 = forms.CharField(
-        label="Password confirmation",
-        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
-        strip=False,
-        help_text="Enter the same password as before, for verification.",
-    )
-
-    def __init__(self, *args, **kwargs):
-        self.instance = UserProfile()
-        self.user = kwargs.pop("user")
-        print(self.user)
-        super().__init__(*args, **kwargs)
-
-    def clean_password2(self):
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise ValidationError(
-                self.error_messages["password_mismatch"],
-                code="password_mismatch",
-            )
-        return password2
 
     def clean_old_password(self):
         old_password = self.cleaned_data.get("old_password")
         if old_password:
             up = UserProfile.get_user(self.user.username)
             if not bcrypt.checkpw(old_password.encode("utf-8"), up.password):
-                self.add_error("old_password", "The old password is incorrect")
+                raise ValidationError("incorrect_old", self.error_messages["incorrect_old"])
         return old_password
-
-
-    def _post_clean(self):
-        super()._post_clean()
-        # Validate the password after self.instance is updated with form data
-        # by super().
-        password = self.cleaned_data.get("password2")
-        if password:
-            try:
-                password_validation.validate_password(password, self.instance)
-            except ValidationError as error:
-                self.add_error("password2", error)
 
     def save(self, username):
         password = self.cleaned_data.get("password2")
         user = UserProfile.get_user(username)
         user.update_password(password)
+
+class UpdateUserProfileForm(forms.Form):
+    country = forms.CharField(widget=forms.Select(choices=build_country_list()))
+
+    def save(self, username):
+        user = UserProfile.get_user(username)
+        user.country = self.cleaned_data.get("country")
+        user.save()
