238babde5f581d796dee6e04927aa7f2f5db861a
Nicolas Lurkin <nicolas.lurkin@cern.ch>
2023-05-06 13:54:03 +0200

    Period is a loosely defined bases on season rather than months

--- BonsaiBuddy_web/BonsaiAdvice/forms.py
+++ BonsaiBuddy_web/BonsaiAdvice/forms.py
@@ -1,12 +1,14 @@
 from django import forms
 from TreeInfo.models import TreeInfo
-import calendar
 
 def build_tree_list():
     return [(_.name.lower(), _.name) for _ in TreeInfo.objects.all()]
 
 def build_periods():
-    return [(_, f"{calendar.month_abbr[(_*2)+1]}-{calendar.month_abbr[(_+1)*2]}") for _ in range(6)]
+    seasons = ["Spring", "Summer", "Autumn", "Winter"]
+    subsection = ["Early", "Late"]
+    return [(f"{iseason}_{isub}", f"{sub} {season}") for isub, sub in enumerate(subsection) for iseason, season in enumerate(seasons)]
+
 class AdviceConfigForm(forms.Form):
     tree           = forms.CharField(label="Tree species", widget=forms.Select(choices=build_tree_list()))
     objective      = forms.CharField(label="Objective")
