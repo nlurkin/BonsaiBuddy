803a6dd0cc68ba7dc51a6d27faada039fa90ea09
Nicolas Lurkin <nicolas.lurkin@cern.ch>
2023-05-06 17:23:31 +0200

    Use TagifyWidget for 'when'
    Requires adding of form.media in header, so add special block

--- BonsaiBuddy_web/BonsaiAdvice/forms.py
+++ BonsaiBuddy_web/BonsaiAdvice/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from TreeInfo.models import TreeInfo
+from .models import BonsaiWhen
+from BonsaiBuddy.widgets import TagifyWidget
 
 def build_tree_list():
     return [(_.name.lower(), _.name) for _ in TreeInfo.objects.all()]
@@ -9,10 +11,13 @@ def build_periods():
     subsection = ["Early", "Late"]
     return [(f"{iseason}_{isub}", f"{sub} {season}") for isub, sub in enumerate(subsection) for iseason, season in enumerate(seasons)]
 
+def build_when():
+    return [(_.short_name, _.display_name) for _ in BonsaiWhen.objects.all()]
+
 class AdviceConfigForm(forms.Form):
     tree           = forms.ChoiceField(label="Tree species", choices=build_tree_list())
     objective      = forms.CharField(label="Objective")
     period         = forms.ChoiceField(label="Period", choices=[(None, "Undefined")] + build_periods())
-    when           = forms.CharField(label="When")
+    when           = forms.ChoiceField(label="When", choices=build_when(), widget=TagifyWidget())
 
 
