289c1650e8c26477973be39b21cf445f87f2e0a7
Nicolas Lurkin <nicolas.lurkin@cern.ch>
2023-05-07 18:55:38 +0200

    Add maxTags to widget

--- BonsaiBuddy_web/BonsaiBuddy/widgets.py
+++ BonsaiBuddy_web/BonsaiBuddy/widgets.py
@@ -5,6 +5,8 @@ def python_to_js(pydict):
     if isinstance(pydict, dict):
         return ", ".join([f"{k}: {{{python_to_js(v)}}}" if isinstance(v, dict) else f"{k}: {python_to_js(v)}" for k, v in pydict.items()]) + ", "
     elif isinstance(pydict, str):
+        if pydict == "Infinity":
+            return pydict
         return f"'{pydict}'"
     elif isinstance(pydict, bool):
         return str(pydict).lower()
@@ -21,6 +23,7 @@ class TagifyWidget(forms.SelectMultiple):
         self.minCharForSuggestions = kwargs.pop("minCharForSuggestions", 0)
         self.closeOnSelect = kwargs.pop("closeOnSelect", False)
         self.maxItems = kwargs.pop("maxItems", 20)
+        self.maxTags = kwargs.pop("maxTags", "Infinity")
         self.enforceList = kwargs.pop("enforceList", True)
         self.editable = kwargs.pop("editable", False)
         super().__init__(*args, **kwargs)
@@ -36,6 +39,7 @@ class TagifyWidget(forms.SelectMultiple):
         return {"editTags": False if self.enforceList else self.editable, # No sense to make the tags editable if the use has no choice anyways
                 "enforceWhitelist": self.enforceList,
                 "tagTextProp": "name",
+                "maxTags": self.maxTags,
                 }
 
     def get_context(self, name, value, attrs):
