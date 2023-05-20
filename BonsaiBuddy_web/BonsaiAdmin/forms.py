8abb1542e1e297fbec595e64e4745185991a3d0a
Nicolas Lurkin <nicolas.lurkin@cern.ch>
2023-05-20 12:14:11 +0200

    Create a base UpdateCreateForm to go with the  UpdateCreateView
    Old forms are all inheriting

--- BonsaiBuddy_web/BonsaiAdmin/forms.py
+++ BonsaiBuddy_web/BonsaiAdmin/forms.py
@@ -3,8 +3,9 @@ from TreeInfo.models import TreeInfo, TechniqueMapper
 from BonsaiAdvice.models import BonsaiTechnique, BonsaiObjective, BonsaiWhen
 from utils import build_technique_categories, build_periods, build_tree_list, build_techniques, build_objectives, build_when
 from BonsaiBuddy.widgets import SelectPlaceholder, TagifyWidget
+from BonsaiBuddy.forms import CreateUpdateForm
 
-class TreeInfoForm(forms.Form):
+class TreeInfoForm(CreateUpdateForm):
     display_name   = forms.CharField(max_length=200, label="Tree name")
     name           = forms.CharField(widget=forms.HiddenInput, required=False)
     latin_name     = forms.CharField(max_length=200)
@@ -17,79 +18,67 @@ class TreeInfoForm(forms.Form):
     propagation    = forms.CharField(widget=forms.Textarea, required=False)
     pests          = forms.CharField(widget=forms.Textarea, required=False)
     published      = forms.BooleanField(initial=False, required=False)
-    update         = forms.BooleanField(initial=False, widget=forms.HiddenInput, required=False)
 
     def clean_name(self):
         # Name is just the lower case display name
         display_name = self.cleaned_data.get("display_name")
         return display_name.lower()
 
-    def create_update(self):
-        update_val = self.cleaned_data["update"]
-        del self.cleaned_data["update"]
-        if update_val:
-            pk = self.cleaned_data["name"]
-            original = TreeInfo.get(pk)
-            original.update(**self.cleaned_data)
-        else:
-            q = TreeInfo(**self.cleaned_data)
-            q.save()
+    def update_object(self):
+        pk = self.cleaned_data["name"]
+        original = TreeInfo.get(pk)
+        original.update(**self.cleaned_data)
 
-class BonsaiTechniqueForm(forms.Form):
+    def create_object(self):
+        q = TreeInfo(**self.cleaned_data)
+        q.save()
+
+class BonsaiTechniqueForm(CreateUpdateForm):
     short_name = forms.CharField(max_length=200)
     display_name = forms.CharField(max_length=200)
     category = forms.ChoiceField(choices=build_technique_categories(), widget=SelectPlaceholder)
     description = forms.CharField(widget=forms.Textarea, required=False)
     published = forms.BooleanField(initial=False, required=False)
-    update = forms.BooleanField(initial=False, widget=forms.HiddenInput, required=False)
 
-    def create_update(self):
-        update_val = self.cleaned_data["update"]
-        del self.cleaned_data["update"]
-        if update_val:
-            pk = self.cleaned_data["short_name"]
-            original = BonsaiTechnique.get(pk)
-            original.update(**self.cleaned_data)
-        else:
-            q = BonsaiTechnique(**self.cleaned_data)
-            q.save()
+    def update_object(self):
+        pk = self.cleaned_data["short_name"]
+        original = BonsaiTechnique.get(pk)
+        original.update(**self.cleaned_data)
 
-class BonsaiObjectiveForm(forms.Form):
+    def create_object(self):
+        q = BonsaiTechnique(**self.cleaned_data)
+        q.save()
+
+class BonsaiObjectiveForm(CreateUpdateForm):
     short_name = forms.CharField(max_length=200)
     display_name = forms.CharField(max_length=200)
     description = forms.CharField(widget=forms.Textarea, required=False)
     published = forms.BooleanField(initial=False, required=False)
-    update = forms.BooleanField(initial=False, widget=forms.HiddenInput, required=False)
 
-    def create_update(self):
-        update_val = self.cleaned_data["update"]
-        del self.cleaned_data["update"]
-        if update_val:
-            pk = self.cleaned_data["short_name"]
-            original = BonsaiObjective.get(pk)
-            original.update(**self.cleaned_data)
-        else:
-            q = BonsaiObjective(**self.cleaned_data)
-            q.save()
+    def update_object(self):
+        pk = self.cleaned_data["short_name"]
+        original = BonsaiObjective.get(pk)
+        original.update(**self.cleaned_data)
 
-class BonsaiWhenForm(forms.Form):
+    def create_object(self):
+        q = BonsaiObjective(**self.cleaned_data)
+        q.save()
+
+class BonsaiWhenForm(CreateUpdateForm):
     short_name = forms.CharField(max_length=200)
     display_name = forms.CharField(max_length=200)
     global_period = forms.ChoiceField(choices=build_periods(), required=False, widget=SelectPlaceholder)
     description = forms.CharField(widget=forms.Textarea, required=False)
     published = forms.BooleanField(initial=False, required=False)
-    update = forms.BooleanField(initial=False, widget=forms.HiddenInput, required=False)
 
-    def create_update(self):
-        update_val = self.cleaned_data["update"]
-        del self.cleaned_data["update"]
-        if update_val:
-            pk = self.cleaned_data["short_name"]
-            original = BonsaiWhen.get(pk)
-            original.update(**self.cleaned_data)
-        else:
-            q = BonsaiWhen(**self.cleaned_data)
-            q.save()
+    def update_object(self):
+        pk = self.cleaned_data["short_name"]
+        original = BonsaiWhen.get(pk)
+        original.update(**self.cleaned_data)
+
+    def create_object(self):
+        q = BonsaiWhen(**self.cleaned_data)
+        q.save()
 
 class TechniqueAssociationForm(forms.Form):
     class Media:
