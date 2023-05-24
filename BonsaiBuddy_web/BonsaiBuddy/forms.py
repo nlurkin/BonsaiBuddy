1b078bd8aa294fcc1786b2cbe2e66ad956382a0b
Nicolas Lurkin <nicolas.lurkin@cern.ch>
2023-05-24 21:07:18 +0200

    Add a delete tick box on the form and forward to delete_object method if ticked

--- BonsaiBuddy_web/BonsaiBuddy/forms.py
+++ BonsaiBuddy_web/BonsaiBuddy/forms.py
@@ -3,11 +3,17 @@ from django import forms
 
 class CreateUpdateForm(forms.Form):
     update = forms.BooleanField(initial=False, widget=forms.HiddenInput, required=False)
+    delete = forms.BooleanField(initial=False, required=False)
+
 
     def create_update(self, **kwargs):
         update_val = self.cleaned_data["update"]
+        delete_val = self.cleaned_data["delete"]
         del self.cleaned_data["update"]
-        if update_val:
+        del self.cleaned_data["delete"]
+        if update_val and delete_val:
+            self.delete_object(**kwargs)
+        elif update_val and not delete_val:
             self.update_object(**kwargs)
         else:
             self.create_object(**kwargs)
