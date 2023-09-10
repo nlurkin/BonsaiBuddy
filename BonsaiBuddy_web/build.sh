f1f4957a3c17b025618191e9b089c09331a92ef3
Nicolas Lurkin <nicolas.lurkin@cern.ch>
2023-09-10 12:08:07 +0200

    Specify settings in migrate

--- BonsaiBuddy_web/build.sh
+++ BonsaiBuddy_web/build.sh
@@ -3,4 +3,4 @@
 pip install -r requirements.txt
 cp /etc/secrets/production.py BonsaiBuddy/settings/
 python manage.py collectstatic --no-input --settings BonsaiBuddy.settings.production
-python manage.py migrate
+python manage.py migrate --settings BonsaiBuddy.settings.production
