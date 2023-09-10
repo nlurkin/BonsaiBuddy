7e11e35acc5e07784e68acc42d1c30bb9e61f916
Nicolas Lurkin <nicolas.lurkin@cern.ch>
2023-09-10 12:22:10 +0200

    Add creation of superuser

--- BonsaiBuddy_web/build.sh
+++ BonsaiBuddy_web/build.sh
@@ -2,5 +2,6 @@
 
 pip install -r requirements.txt
 cp /etc/secrets/production.py BonsaiBuddy/settings/
-python manage.py collectstatic --no-input --settings BonsaiBuddy.settings.production
-python manage.py migrate --settings BonsaiBuddy.settings.production
+python manage.py createsuperuser --no-input
+python manage.py collectstatic --no-input
+python manage.py migrate
