fa7c6a2e7c73b03f473b28ecac977375ba242ec4
Nicolas Lurkin <nicolas.lurkin@gmail.com>
2023-09-16 19:54:42 +0200

    Add a copy button for associations

--- BonsaiBuddy_web/BonsaiAdmin/static/BonsaiAdmin/dyn_form.js
+++ BonsaiBuddy_web/BonsaiAdmin/static/BonsaiAdmin/dyn_form.js
@@ -5,38 +5,41 @@ function updateElementIndex(el, prefix, ndx) {
     if (el.id) el.id = el.id.replace(id_regex, replacement);
     if (el.name) el.name = el.name.replace(id_regex, replacement);
 }
-function cloneMore(selector, prefix) {
+function cloneMore(selector, prefix, reset=true, copied_index=-1) {
     var newElement = $(selector).clone(true);
     var total = $('#id_' + prefix + '-TOTAL_FORMS').val();
+    if (copied_index==-1) {
+        copied_index = total-1;
+    }
     newElement.find(':input:not([type=button]):not([type=submit]):not([type=reset]):not(button)').each(function() {
-        var name = $(this).attr('name').replace('-' + (total-1) + '-', '-' + total + '-');
+        var name = $(this).attr('name').replace('-' + copied_index + '-', '-' + total + '-');
         var id = 'id_' + name;
-        if($(this).attr('name').indexOf("tree_name")==-1)
-            $(this).attr({'name': name, 'id': id}).val('').removeAttr('checked');
+        if(reset && $(this).attr('name').indexOf("tree_name")==-1)
+            $(this).attr({'name': name, 'id': id}) .val('').removeAttr('checked');
         else
             $(this).attr({'name': name, 'id': id});
     });
     newElement.find('label').each(function() {
         var forValue = $(this).attr('for');
         if (forValue) {
-          forValue = forValue.replace('-' + (total-1) + '-', '-' + total + '-');
+          forValue = forValue.replace('-' + copied_index + '-', '-' + total + '-');
           $(this).attr({'for': forValue});
         }
     });
     newElement.find('div.accordion-collapse').each(function() {
-        var id = $(this).attr('id').replace('-' + (total-1) + '-', '-' + total + '-');
+        var id = $(this).attr('id').replace('-' + copied_index + '-', '-' + total + '-');
         $(this).attr({'id': id, 'class': 'accordion-collapse collapse show'});
     });
     newElement.find('button.accordion-button').each(function() {
-        var target = $(this).attr('data-bs-target').replace('-' + (total-1) + '-', '-' + total + '-');
-        var controls = $(this).attr('aria-controls').replace('-' + (total-1) + '-', '-' + total + '-');
+        var target = $(this).attr('data-bs-target').replace('-' + copied_index + '-', '-' + total + '-');
+        var controls = $(this).attr('aria-controls').replace('-' + copied_index + '-', '-' + total + '-');
         $(this).attr({'data-bs-target': target, 'aria-controls': controls, 'class': 'accordion-button', 'aria-expanded': "true"});
         $(this).html("Association #" + total)
     });
-    $(selector).after(newElement);
+    $('.form-row:last').after(newElement);
     newElement.find('tags.tagify').each(function() {
         script = $(this).siblings("script")[0];
-        script.innerHTML = script.innerHTML.replace('-' + (total-1) + '-', '-' + total + '-');
+        script.innerHTML = script.innerHTML.replace('-' + copied_index + '-', '-' + total + '-');
         eval(script.innerHTML);
         $(this).remove();
     });
