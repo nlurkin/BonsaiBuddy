ee0803eb14701c4c4c34ee8a84169ba9b0d23553
Nicolas Lurkin <nicolas.lurkin@b12-consulting.com>
2023-12-28 22:03:27 +0100

    Some formatting

--- BonsaiBuddy_web/TreeInfo/views.py
+++ BonsaiBuddy_web/TreeInfo/views.py
@@ -19,11 +19,13 @@ class IndexView(TreeInfoMenuMixin, generic.ListView):
 
     def get_queryset(self):
         """Return the complete list of available trees."""
-        show_unpublished = user_has_any_perms(self.request.user, ["TreeInfo.change_content"])
+        show_unpublished = user_has_any_perms(
+            self.request.user, ["TreeInfo.change_content"])
         treeinfo = TreeInfo.objects
         if not show_unpublished:
             treeinfo = treeinfo.filter(published=True)
         return treeinfo.order_by("name")
+
 
 class DetailView(TreeInfoMenuMixin, View):
     model = TreeInfo
@@ -36,7 +38,8 @@ class DetailView(TreeInfoMenuMixin, View):
                                                     "technique_table": self.build_technique_table(tree)})
 
     def build_technique_table(self, tree):
-        show_unpublished = user_has_any_perms(self.request.user, ["TreeInfo.change_content"])
+        show_unpublished = user_has_any_perms(
+            self.request.user, ["TreeInfo.change_content"])
 
         # Fetch all associations for the tree
         tech_associations = tree.techniques
@@ -48,14 +51,16 @@ class DetailView(TreeInfoMenuMixin, View):
         # Take all distinct objectives and sort them according to sequence
         # Consider that several may share the same sequence, so use the name
         # also in the index
-        all_objectives = {(_.objective_f.sequence, _.objective_f.short_name): _.objective_f.display_name for _ in tech_associations}
-        all_objectives_ordered = [all_objectives[_] for _ in sorted(all_objectives)]
+        all_objectives = {(_.objective_f.sequence, _.objective_f.short_name)                          : _.objective_f.display_name for _ in tech_associations}
+        all_objectives_ordered = [all_objectives[_]
+                                  for _ in sorted(all_objectives)]
 
         # Prepare the table (header row)
         table = [[""] + all_objectives_ordered]
 
         technique_seen = []
-        techniques_by_category = {_.technique_f.category: [] for _ in tech_associations}
+        techniques_by_category = {_.technique_f.category: []
+                                  for _ in tech_associations}
         for association in tech_associations:
             # Do not repeat techniques appearing multiple times
             if association.technique in technique_seen:
@@ -65,20 +70,24 @@ class DetailView(TreeInfoMenuMixin, View):
                 continue
 
             # Search for all associations sharing the same technique
-            similar = [_ for _ in tech_associations if _.technique == association.technique]
+            similar = [_ for _ in tech_associations if _.technique ==
+                       association.technique]
 
             # Categorize them according to their objective
             objectives = {_: [] for _ in all_objectives_ordered}
             for technique in similar:
                 stage = ",".join(technique.stage_f)
-                periods = ", ".join([periodid_to_name(_) for _ in technique.period])
+                periods = ", ".join([periodid_to_name(_)
+                                    for _ in technique.period])
                 timing = periods
-                if len(stage)>0:
+                if len(stage) > 0:
                     timing += f" ({stage})"
                 # But for each save only the timing and the link to the full description
-                objectives[technique.objective_f.display_name].append(timing + " " + technique.link(tree.name))
+                objectives[technique.objective_f.display_name].append(
+                    timing + " " + technique.link(tree.name))
             # Add the technique as a row in the table
-            techniques_by_category[association.technique_f.category].append([association.technique_f.link()] + ["\n".join(objectives[_]) for _ in all_objectives_ordered])
+            techniques_by_category[association.technique_f.category].append(
+                [association.technique_f.link()] + ["\n".join(objectives[_]) for _ in all_objectives_ordered])
 
         for category in techniques_by_category:
             table.append([category.capitalize()])
