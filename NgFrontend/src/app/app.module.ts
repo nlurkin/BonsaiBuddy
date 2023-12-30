0dbbdb72b5aeea63c33210be2a5f20a782bb5df4
Nicolas Lurkin <nicolas.lurkin@b12-consulting.com>
2023-12-30 18:22:48 +0100

    Add NgLet directive

--- NgFrontend/src/app/app.module.ts
+++ NgFrontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgLetDirective } from './Directives/ng-let.directive';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NgLetDirective,
   ],
   imports: [
     BrowserModule,
