64ad9ada1741418f53efee99edf0a1bba9d7659a
Nicolas Lurkin <nicolas.lurkin@b12-consulting.com>
2024-01-01 13:13:49 +0100

    Add a menu

---
+++ NgFrontend/src/app/Components/Menu/menu/menu.component.ts
@@ -0,0 +1,85 @@
+import { Component } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { AuthenticationService } from 'src/app/Services/authentication.service';
+
+interface MenuItem {
+  label: string;
+  routerLink?: string;
+  action?: () => void;
+  requireLogin: boolean;
+  requireRole?: [];
+}
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss'],
+})
+export class MenuComponent {
+  public sidebarVisible = false;
+  public userName$ = this.authService
+    .getLoggedInUser()
+    .pipe(map((user) => user?.username));
+
+  constructor(private authService: AuthenticationService) {}
+
+  private menuItems: MenuItem[] = [
+    {
+      label: 'Home',
+      routerLink: '/',
+      requireLogin: false,
+    },
+    {
+      label: 'Trees',
+      routerLink: '/treeinfo',
+      requireLogin: false,
+    },
+    // {
+    //   label: 'Advices',
+    //   routerLink: '/advices',
+    //   requireLogin: false,
+    // },
+    // {
+    //   label: 'Profile',
+    //   routerLink: '/profile',
+    //   requireLogin: true,
+    // },
+    // {
+    //   label: 'My Trees',
+    //   routerLink: '/mytrees',
+    //   requireLogin: true,
+    // },
+    // {
+    //   label: 'Admin',
+    //   routerLink: '/admin',
+    //   requireLogin: true,
+    //   requireRole: ['CanChangeContent'],
+    // },
+    {
+      label: 'Login',
+      routerLink: '/login',
+      requireLogin: false,
+    },
+    {
+      label: 'Logout',
+      action: this.logout.bind(this),
+      requireLogin: true,
+    },
+  ];
+
+  public accessibleMenuItems$: Observable<MenuItem[]> = this.authService
+    .isUserLoggedIn()
+    .pipe(
+      map((isLoggedIn) =>
+        this.menuItems.filter((item) => {
+          if (item.label === 'Login' && isLoggedIn) return false;
+          return !item.requireLogin || isLoggedIn;
+        })
+      )
+    );
+
+  private logout() {
+    console.log('logout', this.authService);
+    this.authService.logOut();
+  }
+}
--- NgFrontend/src/app/app.module.ts
+++ NgFrontend/src/app/app.module.ts
@@ -4,17 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuComponent } from './Components/Menu/menu/menu.component';
 import { NgLetDirective } from './Directives/ng-let.directive';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AuthenticationComponent } from './Components/Users/authentication/authentication.component';
 import { ButtonModule } from 'primeng/button';
 import { DividerModule } from 'primeng/divider';
+import { SidebarModule } from 'primeng/sidebar';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { JwtInterceptor } from './jwt.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
+    MenuComponent,
     NgLetDirective,
     AuthenticationComponent,
   ],
@@ -27,6 +30,7 @@ import { JwtInterceptor } from './jwt.interceptor';
     DividerModule,
     FormsModule,
     ReactiveFormsModule,
+    SidebarModule,
   ],
   providers: [
     {
