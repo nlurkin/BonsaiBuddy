bec283abb82effa65cede0b6ff4b5344fe4f2b87
Nicolas Lurkin <nicolas.lurkin@b12-consulting.com>
2024-01-28 00:47:33 +0100

    Generalize advice service methods

--- NgFrontend/src/app/Services/advice.service.ts
+++ NgFrontend/src/app/Services/advice.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { createStore } from '@ngneat/elf';
+import { Store, createStore } from '@ngneat/elf';
 import {
   addEntities,
   deleteEntitiesByPredicate,
@@ -20,49 +20,183 @@ import {
   take,
   tap,
 } from 'rxjs';
-import { AdvicesAPI, BonsaiTechnique } from 'swagger-client';
+import {
+  AdvicesAPI,
+  BonsaiObjective,
+  BonsaiStage,
+  BonsaiTechnique,
+} from 'swagger-client';
 
 const techniqueStore = createStore(
   { name: 'techniques' },
   withEntities<BonsaiTechnique>()
 );
+type StoreType<T> = Store<
+  {
+    name: string;
+    state: { entities: Record<string, T>; ids: string[] };
+    config: { idKey: 'id' };
+  },
+  { entities: Record<string, T>; ids: string[] }
+>;
 
 type StoreCommands = 'update' | 'create' | 'delete';
+type BonsaiEntity = BonsaiTechnique | BonsaiObjective | BonsaiStage;
 
 @Injectable({
   providedIn: 'root',
 })
 export class AdviceService {
-  private readonly refresh$: BehaviorSubject<void> = new BehaviorSubject<void>(
-    undefined
-  ); // To force the refresh of the store
+  private readonly refreshTechnique$: BehaviorSubject<void> =
+    new BehaviorSubject<void>(undefined); // To force the refresh of the store
 
   constructor(private techniqueApiService: AdvicesAPI) {}
 
   /**
+   * Generic methods
+   */
+  private initializeStore<T>(
+    refresh$: BehaviorSubject<void>,
+    retrieveFn: () => Observable<T[]>,
+    store: StoreType<T>
+  ): Observable<boolean> {
+    return refresh$.pipe(
+      switchMap(() =>
+        retrieveFn
+          .bind(this.techniqueApiService)()
+          .pipe(
+            take(1),
+            map((entities) => {
+              store.update(setEntities(entities));
+              return true;
+            })
+          )
+      ),
+      shareReplay(1)
+    );
+  }
+  private fetchEntity<T extends BonsaiEntity>(
+    id: string,
+    retrieveFn: (id: string) => Observable<T>,
+    store: StoreType<T>
+  ): Observable<T | undefined> {
+    retrieveFn
+      .bind(this.techniqueApiService)(id)
+      .pipe(take(1))
+      .subscribe((entity: T) => {
+        store.update(addEntities(entity));
+      });
+    return store.pipe(selectEntity(id));
+  }
+
+  public getEntity<T extends BonsaiEntity>(
+    entity_short_name: string,
+    retrieveFn: (id: string) => Observable<T>,
+    storeInitialized$: Observable<boolean>,
+    store: StoreType<T>
+  ): Observable<T | undefined> {
+    return storeInitialized$.pipe(
+      switchMap(() =>
+        store.pipe(
+          selectEntityByPredicate(
+            ({ short_name }) => short_name === entity_short_name
+          ),
+          switchMap((entity) =>
+            entity
+              ? of(entity)
+              : this.fetchEntity<T>(entity_short_name, retrieveFn, store)
+          )
+        )
+      )
+    );
+  }
+
+  public updateEntity<T extends BonsaiEntity>(
+    entity: T,
+    updateFn: (short_name: string, e: T) => Observable<T>,
+    store: StoreType<T>
+  ): Observable<T> {
+    return updateFn
+      .bind(this.techniqueApiService)(entity.short_name, entity)
+      .pipe(
+        take(1),
+        tap((updatedEntity) =>
+          this.updateStore<T>(updatedEntity, store, 'update')
+        )
+      );
+  }
+
+  public createEntity<T extends BonsaiEntity>(
+    entity: T,
+    createFn: (e: T) => Observable<T>,
+    store: StoreType<T>
+  ): Observable<T> {
+    return createFn
+      .bind(this.techniqueApiService)(entity)
+      .pipe(
+        take(1),
+        tap((entity) => this.updateStore<T>(entity, store, 'create'))
+      );
+  }
+
+  public deleteEntity<T extends BonsaiEntity>(
+    entity_short_name: string,
+    deleteFn: (short_name: string) => Observable<any>,
+    store: StoreType<T>
+  ): Observable<any> {
+    return deleteFn
+      .bind(this.techniqueApiService)(entity_short_name)
+      .pipe(
+        take(1),
+        tap(() => this.deleteFromStore<T>(entity_short_name, store))
+      );
+  }
+
+  private updateStore<T extends BonsaiEntity>(
+    technique: T,
+    store: StoreType<T>,
+    command: StoreCommands
+  ): void {
+    if (command === 'update')
+      store.update(
+        updateEntitiesByPredicate(
+          ({ short_name }) => short_name === technique.short_name,
+          () => technique
+        )
+      );
+    else if (command === 'create') store.update(addEntities([technique]));
+    else if (command === 'delete')
+      this.deleteFromStore<T>(technique.short_name, store);
+  }
+
+  private deleteFromStore<T extends BonsaiEntity>(
+    entity_short_name: string,
+    store: StoreType<T>
+  ): void {
+    store.update(
+      deleteEntitiesByPredicate(
+        ({ short_name }) => short_name === entity_short_name
+      )
+    );
+  }
+
+  /**
    * Interactions with the API
    */
-  private initializedStore$: Observable<boolean> = this.refresh$.pipe(
-    switchMap(() =>
-      this.techniqueApiService.advicesTechniquesList().pipe(
-        take(1),
-        map((techniques) => {
-          techniqueStore.update(setEntities(techniques));
-          return true;
-        })
-      )
-    ),
-    shareReplay(1)
+  private initializedTechniqueStore$ = this.initializeStore(
+    this.refreshTechnique$,
+    this.techniqueApiService.advicesTechniquesList,
+    techniqueStore
   );
 
   private fetchTechnique(id: string): Observable<BonsaiTechnique | undefined> {
-    this.techniqueApiService
-      .advicesTechniquesRetrieve(id)
-      .pipe(take(1))
-      .subscribe((techniques) => {
-        techniqueStore.update(addEntities(techniques));
-      });
-    return techniqueStore.pipe(selectEntity(id));
+    return this.fetchEntity<BonsaiTechnique>(
+      id,
+      this.techniqueApiService.advicesTechniquesRetrieve,
+      techniqueStore
+    );
+  }
+
   }
 
   public getTechniqueCategories(): Observable<string[]> {
@@ -72,75 +206,49 @@ export class AdviceService {
   public updateTechnique(
     technique: BonsaiTechnique
   ): Observable<BonsaiTechnique> {
-    return this.techniqueApiService
-      .advicesTechniquesUpdate(technique.short_name, technique)
-      .pipe(
-        take(1),
-        tap((technique) => this.updateStore(technique, 'update'))
-      );
+    return this.updateEntity(
+      technique,
+      this.techniqueApiService.advicesTechniquesUpdate,
+      techniqueStore
+    );
   }
 
   public createTechnique(
     technique: BonsaiTechnique
   ): Observable<BonsaiTechnique> {
-    return this.techniqueApiService.advicesTechniquesCreate(technique).pipe(
-      take(1),
-      tap((technique) => this.updateStore(technique, 'create'))
+    return this.createEntity(
+      technique,
+      this.techniqueApiService.advicesTechniquesCreate,
+      techniqueStore
     );
   }
 
   public deleteTechnique(technique_short_name: string): Observable<any> {
-    return this.techniqueApiService
-      .advicesTechniquesDestroy(technique_short_name)
-      .pipe(
-        take(1),
-        tap(() => this.deleteFromStore(technique_short_name))
-      );
+    return this.deleteEntity(
+      technique_short_name,
+      this.techniqueApiService.advicesTechniquesDestroy,
+      techniqueStore
+    );
+  }
+
   }
 
   /**
    * Interactions with the store
    */
   public getTechniques(): Observable<BonsaiTechnique[]> {
-    return this.initializedStore$.pipe(
+    return this.initializedTechniqueStore$.pipe(
       switchMap(() => techniqueStore.pipe(selectAllEntities()))
     );
   }
 
   public getTechnique(name: string): Observable<BonsaiTechnique | undefined> {
-    return this.initializedStore$.pipe(
-      switchMap(() =>
-        techniqueStore.pipe(
-          selectEntityByPredicate(({ short_name }) => short_name === name),
-          switchMap((entity) =>
-            entity ? of(entity) : this.fetchTechnique(name)
-          )
-        )
-      )
+    return this.getEntity<BonsaiTechnique>(
+      name,
+      this.techniqueApiService.advicesTechniquesRetrieve,
+      this.initializedTechniqueStore$,
+      techniqueStore
     );
   }
 
-  private updateStore(
-    technique: BonsaiTechnique,
-    command: StoreCommands
-  ): void {
-    if (command === 'update')
-      techniqueStore.update(
-        updateEntitiesByPredicate(
-          ({ short_name }) => short_name === technique.short_name,
-          () => technique
-        )
-      );
-    else if (command === 'create')
-      techniqueStore.update(addEntities([technique]));
-    else if (command === 'delete') this.deleteFromStore(technique.short_name);
-  }
-
-  private deleteFromStore(technique_short_name: string): void {
-    techniqueStore.update(
-      deleteEntitiesByPredicate(
-        ({ short_name }) => short_name === technique_short_name
-      )
-    );
-  }
 }
