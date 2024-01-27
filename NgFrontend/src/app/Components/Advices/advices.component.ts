6ee05708cc094a2fc3c0d10fa4074b62a653bd78
Nicolas Lurkin <nicolas.lurkin@b12-consulting.com>
2024-01-28 00:48:46 +0100

    Display objectives and stages

--- NgFrontend/src/app/Components/Advices/advices.component.ts
+++ NgFrontend/src/app/Components/Advices/advices.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Observable, map } from 'rxjs';
 import { AdviceService } from 'src/app/Services/advice.service';
 import * as _ from 'lodash';
-import { BonsaiTechnique } from 'swagger-client';
+import { BonsaiObjective, BonsaiStage, BonsaiTechnique } from 'swagger-client';
 
 @Component({
   selector: 'app-advices',
@@ -21,6 +21,12 @@ export class AdvicesComponent {
         _.groupBy(techniques, (technique) => technique.category)
       )
     );
+
+  public readonly objectives$: Observable<BonsaiObjective[]> =
+    this.adviceService.getObjectives();
+
+  public readonly stages$: Observable<BonsaiStage[]> =
+    this.adviceService.getStages();
 
   constructor(private adviceService: AdviceService) {}
 }
