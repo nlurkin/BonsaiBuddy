0bf507e49161d5fab03548baa18ad8c4d0eb0052
Nicolas Lurkin <nicolas.lurkin@b12-consulting.com>
2024-01-28 15:50:44 +0100

    Provide list of permissions in the user API and limit available info to what is strictly needed

--- BonsaiBuddy_web/BonsaiUsers/serializers.py
+++ BonsaiBuddy_web/BonsaiUsers/serializers.py
@@ -3,7 +3,12 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
+    permissions = serializers.SerializerMethodField()
 
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['id', 'username', 'email', 'permissions',
+                  'last_login', 'first_name', 'last_name', 'is_active']
+
+    def get_permissions(self, obj):
+        return [_ for _ in obj.get_all_permissions()]
--- NgFrontend/projects/swagger-client/src/model/user.ts
+++ NgFrontend/projects/swagger-client/src/model/user.ts
@@ -13,35 +13,18 @@
 
 export interface User { 
     readonly id: number;
-    password: string;
-    last_login?: string | null;
-    /**
-     * Designates that this user has all permissions without explicitly assigning them.
-     */
-    is_superuser?: boolean;
     /**
      * Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.
      */
     username: string;
+    email?: string;
+    readonly permissions: string;
+    last_login?: string | null;
     first_name?: string;
     last_name?: string;
-    email?: string;
-    /**
-     * Designates whether the user can log into this admin site.
-     */
-    is_staff?: boolean;
     /**
      * Designates whether this user should be treated as active. Unselect this instead of deleting accounts.
      */
     is_active?: boolean;
-    date_joined?: string;
-    /**
-     * The groups this user belongs to. A user will get all permissions granted to each of their groups.
-     */
-    groups?: Array<number>;
-    /**
-     * Specific permissions for this user.
-     */
-    user_permissions?: Array<number>;
 }
 
