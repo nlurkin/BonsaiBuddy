5ea00498e6dc16baecb9d655a1601ebfdd006ae0
Nicolas Lurkin <nicolas.lurkin@b12-consulting.com>
2024-01-28 21:30:19 +0100

    Add IsLoggedInGuard

--- NgFrontend/src/app/has-permissions.guard.ts
+++ NgFrontend/src/app/has-permissions.guard.ts
@@ -1,7 +1,12 @@
 import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
 import { UserService } from './Services/user.service';
+import { AuthenticationService } from './Services/authentication.service';
 
 export const hasPermissionsGuard: CanActivateFn = (route, state) => {
   return inject(UserService).currentHasPermissions(route.data['permissions']);
+};
+
+export const isLoggedInGuard: CanActivateFn = (route, state) => {
+  return inject(AuthenticationService).isUserLoggedIn();
 };
