44f66e29b08669f88b0b065a2df210f03972cacf
Nicolas Lurkin <nicolas.lurkin@b12-consulting.com>
2024-02-02 21:02:41 +0100

    Add title to all routes + cleaning

--- NgFrontend/src/app/app.component.ts
+++ NgFrontend/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable, Subject, filter, map, takeUntil } from 'rxjs';
 import { MessageService } from 'primeng/api';
+import { Subject, takeUntil } from 'rxjs';
 import { ToastingService } from './Services/toasting.service';
 
 @Component({
