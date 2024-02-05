866533da9be6456e8ed58ba1a1278bc5b51bbbbb
Nicolas Lurkin <nicolas.lurkin@b12-consulting.com>
2024-02-05 23:08:21 +0100

    Add update methods for treeInfo

--- NgFrontend/src/app/Services/tree-info.service.ts
+++ NgFrontend/src/app/Services/tree-info.service.ts
@@ -2,17 +2,34 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { TreeInfo, TreesAPI } from 'swagger-client';
 
+export type BasicTreeInfo = Omit<TreeInfo, 'techniques'>;
+
 @Injectable({
   providedIn: 'root',
 })
 export class TreeInfoService {
   constructor(private treeInfoService: TreesAPI) {}
 
-  getAllTreeInfo(): Observable<TreeInfo[]> {
+  public getAllTreeInfo(): Observable<TreeInfo[]> {
     return this.treeInfoService.treesList();
   }
 
-  getTreeInfo(id: string): Observable<TreeInfo> {
+  public getTreeInfo(id: string): Observable<TreeInfo> {
     return this.treeInfoService.treesRetrieve(id);
+  }
+
+  public updateBasicTreeInfo(
+    id: string,
+    treeInfo: Partial<BasicTreeInfo>
+  ): Observable<TreeInfo> {
+    return this.treeInfoService.treesPartialUpdate(id, treeInfo);
+  }
+
+  public createTreeInfo(treeInfo: TreeInfo): Observable<TreeInfo> {
+    return this.treeInfoService.treesCreate(treeInfo);
+  }
+
+  public deleteTreeInfo(id: string): Observable<TreeInfo> {
+    return this.treeInfoService.treesDestroy(id);
   }
 }
