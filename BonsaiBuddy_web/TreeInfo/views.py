107d8f14452804d89599035ff11ef81e6aee9aff
nicolas.lurkin <nicolas.lurkin@b12-consulting.com>
2024-05-17 21:25:54 +0200

    Add a print

--- BonsaiBuddy_web/TreeInfo/views.py
+++ BonsaiBuddy_web/TreeInfo/views.py
@@ -1,12 +1,12 @@
 # Create your views here.
-from drf_spectacular.utils import extend_schema, extend_schema_view
-from drf_spectacular.utils import OpenApiParameter
-from BonsaiAdvice.models import periodid_to_name
 from django.shortcuts import render
 from django.views import View, generic
-from rest_framework_mongoengine import viewsets
-from utils import get_object_or_404, user_has_any_perms
+from drf_spectacular.utils import OpenApiParameter, extend_schema, extend_schema_view
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework_mongoengine import viewsets
+
+from BonsaiAdvice.models import periodid_to_name
+from utils import get_object_or_404, user_has_any_perms
 
 from .menu import TreeInfoMenuMixin
 from .models import TreeInfo, TreeInfoPermissionModelAPI
@@ -20,7 +20,8 @@ class IndexView(TreeInfoMenuMixin, generic.ListView):
     def get_queryset(self):
         """Return the complete list of available trees."""
         show_unpublished = user_has_any_perms(
-            self.request.user, ["TreeInfo.change_content"])
+            self.request.user, ["TreeInfo.change_content"]
+        )
         treeinfo = TreeInfo.objects
         if not show_unpublished:
             treeinfo = treeinfo.filter(published=True)
@@ -33,13 +34,22 @@ class DetailView(TreeInfoMenuMixin, View):
     context_object_name = "tree_info"
 
     def get(self, request, pk):
+        print(pk)
         tree = get_object_or_404(self.model, name=pk)
-        return render(request, self.template_name, {**self.build_menu_context(request), self.context_object_name: tree,
-                                                    "technique_table": self.build_technique_table(tree)})
+        return render(
+            request,
+            self.template_name,
+            {
+                **self.build_menu_context(request),
+                self.context_object_name: tree,
+                "technique_table": self.build_technique_table(tree),
+            },
+        )
 
     def build_technique_table(self, tree):
         show_unpublished = user_has_any_perms(
-            self.request.user, ["TreeInfo.change_content"])
+            self.request.user, ["TreeInfo.change_content"]
+        )
 
         # Fetch all associations for the tree
         tech_associations = tree.techniques
@@ -51,16 +61,20 @@ class DetailView(TreeInfoMenuMixin, View):
         # Take all distinct objectives and sort them according to sequence
         # Consider that several may share the same sequence, so use the name
         # also in the index
-        all_objectives = {(_.objective_f.sequence, _.objective_f.short_name): _.objective_f.display_name for _ in tech_associations}
-        all_objectives_ordered = [all_objectives[_]
-                                  for _ in sorted(all_objectives)]
+        all_objectives = {
+            (
+                _.objective_f.sequence,
+                _.objective_f.short_name,
+            ): _.objective_f.display_name
+            for _ in tech_associations
+        }
+        all_objectives_ordered = [all_objectives[_] for _ in sorted(all_objectives)]
 
         # Prepare the table (header row)
         table = [[""] + all_objectives_ordered]
 
         technique_seen = []
-        techniques_by_category = {_.technique_f.category: []
-                                  for _ in tech_associations}
+        techniques_by_category = {_.technique_f.category: [] for _ in tech_associations}
         for association in tech_associations:
             # Do not repeat techniques appearing multiple times
             if association.technique in technique_seen:
@@ -70,24 +84,27 @@ class DetailView(TreeInfoMenuMixin, View):
                 continue
 
             # Search for all associations sharing the same technique
-            similar = [_ for _ in tech_associations if _.technique ==
-                       association.technique]
+            similar = [
+                _ for _ in tech_associations if _.technique == association.technique
+            ]
 
             # Categorize them according to their objective
             objectives = {_: [] for _ in all_objectives_ordered}
             for technique in similar:
                 stage = ",".join(technique.stage_f)
-                periods = ", ".join([periodid_to_name(_)
-                                    for _ in technique.period])
+                periods = ", ".join([periodid_to_name(_) for _ in technique.period])
                 timing = periods
                 if len(stage) > 0:
                     timing += f" ({stage})"
                 # But for each save only the timing and the link to the full description
                 objectives[technique.objective_f.display_name].append(
-                    timing + " " + technique.link(tree.name))
+                    timing + " " + technique.link(tree.name)
+                )
             # Add the technique as a row in the table
             techniques_by_category[association.technique_f.category].append(
-                [association.technique_f.link()] + ["\n".join(objectives[_]) for _ in all_objectives_ordered])
+                [association.technique_f.link()]
+                + ["\n".join(objectives[_]) for _ in all_objectives_ordered]
+            )
 
         for category in techniques_by_category:
             table.append([category.capitalize()])
@@ -96,17 +113,22 @@ class DetailView(TreeInfoMenuMixin, View):
         return table
 
 
-@extend_schema_view(retrieve=extend_schema(parameters=[OpenApiParameter("name", str, OpenApiParameter.PATH)]))
+@extend_schema_view(
+    retrieve=extend_schema(
+        parameters=[OpenApiParameter("name", str, OpenApiParameter.PATH)]
+    )
+)
 class TreeInfoViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows TreeInfo to be viewed or edited.
     """
-    lookup_field = 'name'
+
+    lookup_field = "name"
     serializer_class = TreeInfoSerializer
-    permission_classes = [
-        IsAuthenticatedOrReadOnly, TreeInfoPermissionModelAPI]
+    permission_classes = [IsAuthenticatedOrReadOnly, TreeInfoPermissionModelAPI]
 
     def get_queryset(self):
         show_unpublished = user_has_any_perms(
-            self.request.user, ["TreeInfo.change_content"])
+            self.request.user, ["TreeInfo.change_content"]
+        )
         return TreeInfo.get_all(not show_unpublished).order_by("display_name")
