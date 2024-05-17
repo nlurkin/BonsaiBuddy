cef99adfd6071635dd5be2d33a6cb9e7b1529cd7
nicolas.lurkin <nicolas.lurkin@b12-consulting.com>
2024-05-17 18:46:05 +0200

    No cp

--- BonsaiBuddy_web/build.sh
+++ BonsaiBuddy_web/build.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 
 pip install -r requirements.txt
-cp /etc/secrets/production.py BonsaiBuddy/settings/
+# cp /etc/secrets/production.py BonsaiBuddy/settings/
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py createsuperuser --no-input
