b89c99a45438dbd0443c105147168f546bd6766d
nicolas.lurkin <nicolas.lurkin@b12-consulting.com>
2024-05-17 19:26:44 +0200

    No superuser

--- BonsaiBuddy_web/build.sh
+++ BonsaiBuddy_web/build.sh
@@ -4,4 +4,4 @@ python3 -m pip install -r requirements.txt
 # cp /etc/secrets/production.py BonsaiBuddy/settings/
 python3 manage.py collectstatic --no-input
 python3 manage.py migrate
-python3 manage.py createsuperuser --no-input
+# python3 manage.py createsuperuser --no-input
